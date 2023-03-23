0e1836437e423af1f151672ef61ba152bb6c3b7b
Muhammad Habibi Husni <habibihusni18@gmail.com>
2023-03-23 15:32:01 +0700

    fix: some DFS and BFS bug

--- src/maze/PathResult.cs
+++ src/maze/PathResult.cs
@@ -2,8 +2,8 @@
 {
     internal struct PathResult
     {
-        private List<Coordinate> path { get; } = new List<Coordinate>();
-        private List<Coordinate> searchOrder { get; } = new List<Coordinate>();
+        public List<Coordinate> path = new List<Coordinate>();
+        public List<Coordinate> searchOrder = new List<Coordinate>();
 
         public PathResult(List<Coordinate> path, List<Coordinate> searchOrder)
         {
--- src/maze/Solver.cs
+++ src/maze/Solver.cs
@@ -53,18 +53,15 @@
             var stack = new Stack<Coordinate>();
             int treasureFound = 0;
             stack.Push(start);
+            dikunjungi[start.y, start.x] = true;
             while (stack.Count > 0)
             {
                 var now = stack.Peek();
-                if (!dikunjungi[now.y, now.x])
-                {
-                    searchOrder.Add(now);
-                }
-                path.Add(now);
-                dikunjungi[now.y, now.x] = true;
+                searchOrder.Add(now);
+                path.Add(now);       
                 if (tmpMaze[now.y, now.x] == 'T')
                 {
-                    tmpMaze[now.y, now.x] = 'M';
+                    tmpMaze[now.y, now.x] = 'R';
                     treasureFound++;
                 }
                 if (treasureFound < totalTreasure)
@@ -72,22 +69,28 @@
                     if (now.x + 1 < colLen && !dikunjungi[now.y, now.x + 1] && tmpMaze[now.y, now.x + 1] != 'X')
                     {
                         stack.Push(new Coordinate(now.x + 1, now.y));
+                        dikunjungi[now.y, now.x + 1] = true;
                     }
                     else if (now.y + 1 < rowLen && !dikunjungi[now.y + 1, now.x] && tmpMaze[now.y + 1, now.x] != 'X')
                     {
                         stack.Push(new Coordinate(now.x, now.y + 1));
+                        dikunjungi[now.y + 1, now.x] = true;
                     }
                     else if (now.x - 1 >= 0 && !dikunjungi[now.y, now.x - 1] && tmpMaze[now.y, now.x - 1] != 'X')
                     {
                         stack.Push(new Coordinate(now.x - 1, now.y));
+                        dikunjungi[now.y, now.x - 1] = true;
                     }
                     else if (now.y - 1 >= 0 && !dikunjungi[now.y - 1, now.x] && tmpMaze[now.y - 1, now.x] != 'X')
                     {
                         stack.Push(new Coordinate(now.x, now.y - 1));
+                        dikunjungi[now.y - 1, now.x] = true;
                     }
                     else
                     {
                         stack.Pop();
+                        searchOrder.RemoveAt(searchOrder.Count-1);
+
                     }
                 }
                 else
@@ -116,16 +119,14 @@
             var queue = new Queue<Coordinate>();
             bool goBack = false;
             int foundTreasure = 0;
+
             queue.Enqueue(start);
-            while(queue.Count > 0)
+            visited[start.y, start.x] = true;
+            rute.Add(start);
+            while (queue.Count > 0)
             {
                 var vertex = queue.Dequeue();
-
-                if (visited[vertex.y,vertex.x])
-                    continue;
                 searchOrder.Add(vertex);
-                visited[vertex.y,vertex.x] = true;  
-
                 if ((tmpMaze[vertex.y,vertex.x] == 'T') || 
                     (tmpMaze[vertex.y,vertex.x] ==  'K' && isTSP && goBack))
                 {
@@ -140,9 +141,13 @@
                     queue.Clear();
                     if (tmpMaze[vertex.y, vertex.x] == 'T')
                     {
-                        tmpMaze[vertex.y, vertex.x] = 'M';
+                        tmpMaze[vertex.y, vertex.x] = 'R';
                         foundTreasure++;
-
+                        if (!isThereTetangga(vertex,tmpMaze,visited))
+                        {
+                            visited = new bool[rowLen, colLen];
+                            visited[vertex.y, vertex.x] = true;
+                        }
                     }
                     prevStart = vertex;
                     if (foundTreasure == totalTreasure)
@@ -150,36 +155,55 @@
                         if (isTSP)
                         {
                             goBack = true;
-                            visited = new bool[rowLen, colLen];
                         } else
                         {
                             break;
                         }
                     }
                 }
-                if ((vertex.x + 1 < colLen) && !visited[vertex.y, vertex.x + 1] && (tmpMaze[vertex.y,vertex.x + 1] != 'X'))
+                if ((vertex.x + 1 < colLen) && !visited[vertex.y, vertex.x + 1] && (tmpMaze[vertex.y, vertex.x + 1] != 'X'))
                 {
-                    queue.Enqueue(new Coordinate(vertex.y, vertex.x + 1));
+                    queue.Enqueue(new Coordinate(vertex.x + 1, vertex.y));
                     prevCoor[vertex.y, vertex.x + 1] = vertex;
+                    visited[vertex.y, vertex.x + 1] = true;
                 }
                 if ((vertex.y + 1 < rowLen) && !visited[vertex.y + 1, vertex.x] && (tmpMaze[vertex.y + 1, vertex.x] != 'X'))
                 {
-                    queue.Enqueue(new Coordinate(vertex.y + 1, vertex.x));
+                    queue.Enqueue(new Coordinate(vertex.x, vertex.y + 1));
                     prevCoor[vertex.y + 1, vertex.x] = vertex;
+                    visited[vertex.y + 1, vertex.x] = true;
                 }
-                if ((vertex.y - 1 > 0) && !visited[vertex.y - 1, vertex.x] && (tmpMaze[vertex.y -1 , vertex.x] != 'X'))
+                if ((vertex.y - 1 >= 0) && !visited[vertex.y - 1, vertex.x] && (tmpMaze[vertex.y - 1, vertex.x] != 'X'))
                 {
-                    queue.Enqueue(new Coordinate(vertex.y - 1, vertex.x));
+                    queue.Enqueue(new Coordinate(vertex.x, vertex.y - 1));
                     prevCoor[vertex.y - 1, vertex.x] = vertex;
+                    visited[vertex.y + 1, vertex.x] = true;
                 }
-                if ((vertex.x - 1 > 0) && !visited[vertex.y, vertex.x - 1] && (tmpMaze[vertex.y, vertex.x - 1] != 'X'))
+                if ((vertex.x - 1 >= 0) && !visited[vertex.y, vertex.x - 1] && (tmpMaze[vertex.y, vertex.x - 1] != 'X'))
                 {
-                    queue.Enqueue(new Coordinate(vertex.y, vertex.x - 1));
+                    queue.Enqueue(new Coordinate(vertex.x - 1, vertex.y));
                     prevCoor[vertex.y, vertex.x - 1] = vertex;
+                    visited[vertex.y, vertex.x - 1] = true;
                 }
             }
             PathResult pathResult = new PathResult(rute,searchOrder);
             return pathResult;
+        }
+
+        private bool isThereTetangga(Coordinate c, char[,] m, bool[,] visited)
+        {
+
+            if (((c.x + 1 < colLen) && !visited[c.y, c.x + 1] && (m[c.y, c.x + 1] != 'X'))
+                || ((c.y + 1 < rowLen) && !visited[c.y + 1, c.x] && (m[c.y + 1, c.x] != 'X'))
+                || ((c.y - 1 >= 0) && !visited[c.y - 1, c.x] && (m[c.y - 1, c.x] != 'X'))
+                || ((c.x - 1 >= 0) && !visited[c.y, c.x - 1] && (m[c.y, c.x - 1] != 'X')))  
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+
         }
     }
 }
